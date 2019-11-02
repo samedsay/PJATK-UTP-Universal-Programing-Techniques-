6c7b002c21dfd31b182a12d2c2b52eec6f26ca79
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2019-11-02 12:01:27 +0100

    Add files via upload

---
+++ Generics/Skeleton/Source/Person.java
@@ -0,0 +1,28 @@
+public class Person implements IAggregable<Person, Integer>, IDeeplyCloneable<Person> {
+	
+	private int _age;
+	
+	public Person() {
+	}
+	
+	public Person(int age) {
+		_age = age;
+	}
+	
+	public int age() {
+		return _age;
+	}
+
+	public Integer aggregate(Integer intermediateResult) {
+		if  (intermediateResult == null) {
+			return _age;
+		}
+		return _age + intermediateResult;
+	}
+	
+	public Person deepClone() {
+		Person clone = new Person();
+		clone._age = _age;
+		return clone;
+	}
+}
