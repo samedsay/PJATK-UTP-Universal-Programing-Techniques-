358bd53239ebebb842fa2f1542b14496007c931d
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2019-11-07 18:23:51 +0100

    Create FirstNameComparator.java

---
+++ Collections/Skeleton/Source/FirstNameComparator.java
@@ -0,0 +1,10 @@
+import java.util.Comparator;
+
+public class FirstNameComparator implements Comparator<Person> {
+
+	@Override
+	public int compare(Person person1, Person person2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
