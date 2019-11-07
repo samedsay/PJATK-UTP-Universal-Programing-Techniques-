5bccde7576ce6e810465d49e9653ae48ab240db2
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2019-11-07 18:25:21 +0100

    Create InputParser.java

---
+++ Collections/Skeleton/Source/InputParser.java
@@ -0,0 +1,13 @@
+import java.io.File;
+import java.util.List;
+
+public final class InputParser {
+	
+	// 1. Use regular expresssions (Pattern) for validating input data
+	// 2. Convert input string representing date using SimpleDateFormat "yyyy-MM-dd" 
+	// 3. SimpleDateFormat format "yyyy-MM-dd"
+
+	public static List<Person> parse(File file) {
+		return null;
+	}
+}
