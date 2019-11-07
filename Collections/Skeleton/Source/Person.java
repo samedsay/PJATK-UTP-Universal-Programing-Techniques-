a7b93d9e60faada5a14abbb5232c700cbb486b98
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2019-11-07 18:25:58 +0100

    Create Person.java

---
+++ Collections/Skeleton/Source/Person.java
@@ -0,0 +1,20 @@
+import java.util.Date;
+
+public class Person implements Comparable<Person> {
+	
+	private final String _firstName;
+	private final String _surname;
+	private final Date _birthdate;
+	
+	public Person(String firstName, String surname, Date birthdate) {
+		_firstName = firstName;
+		_surname = surname;
+		_birthdate = birthdate;
+	}
+
+	@Override
+	public int compareTo(Person otherPerson) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
