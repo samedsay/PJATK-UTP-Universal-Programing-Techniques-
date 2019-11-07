51d0ab4038c7228fb6313af6122d37b760d58970
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2019-11-07 18:26:30 +0100

    Create PersonDatabase.java

---
+++ Collections/Skeleton/Source/PersonDatabase.java
@@ -0,0 +1,21 @@
+import java.util.Date;
+import java.util.List;
+
+public final class PersonDatabase {
+
+	public List<Person> sortedByFirstName() {
+		return null;
+	}
+	
+	public List<Person> sortedBySurnameFirstNameAndBirthdate() {
+		return null;
+	}
+	
+	public List<Person> sortedByBirthdate() {
+		return null;
+	}
+	
+	public List<Person> bornOnDay(Date date) {
+		return null;
+	}
+}
