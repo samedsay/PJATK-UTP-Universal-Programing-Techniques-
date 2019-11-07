bec3dab0bedcafbe349e5b2824941f5f32030f57
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2019-11-07 18:23:13 +0100

    Create BirthDateComparator.java

---
+++ Collections/Skeleton/Source/BirthDateComparator.java
@@ -0,0 +1,10 @@
+import java.util.Comparator;
+
+public final class BirthDateComparator implements Comparator<Person> {
+
+	@Override
+	public int compare(Person person1, Person person2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
